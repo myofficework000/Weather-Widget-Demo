5cfd7d14d096b49a620b85c1512ecd72334c5527
cheetahmail007 <cheetahmail007@gmail.com>
2023-02-13 20:26:17 +0530

    updated endpoints

--- app/src/main/java/com/example/weatherwidget/model/remote/Constant.kt
+++ app/src/main/java/com/example/weatherwidget/model/remote/Constant.kt
@@ -4,10 +4,11 @@ package com.example.weatherwidget.model.remote
 This class is responsible to add constant of project
 */
 object Constant {
-    //Luan  //API 1 https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-    //Alex  //API 2 api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
-    //Joshua  //API 3 https://openweathermap.org/api/air-pollution
-    //Thomas  //API 4 http://api.openweathermap.org/geo/1.0/zip?zip={zip code},{country code}&appid={API key}
-
+    const val BASE_URL = "https://api.openweathermap.org/"
     const val API_KEY = "979f405df6be739658dd4ebbde6eb52c"
+    const val END_POINT_WEATHER = "data/2.5/weather?q=Houston&appid=$API_KEY"
+    const val END_POINT_FORECAST = "data/2.5/forecast?q={city name}&appid=$API_KEY"
+    const val END_POINT_AIR_POLLUTION = "data/2.5/air_pollution/forecast"
+    const val END_POINT_ZIPCODE = "geo/1.0/zip?zip=81658&appid=$API_KEY"
+
 }
