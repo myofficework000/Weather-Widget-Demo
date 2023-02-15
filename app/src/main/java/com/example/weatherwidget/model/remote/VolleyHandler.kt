10a0ebd79aff98ee9252b2f8a2cd17d82cc3e982
Luan Dang <luandang9697@gmail.com>
2023-02-15 14:02:08 -0600

    Updated Weather Fragment

--- app/src/main/java/com/example/weatherwidget/model/remote/VolleyHandler.kt
+++ app/src/main/java/com/example/weatherwidget/model/remote/VolleyHandler.kt
@@ -18,24 +18,24 @@ import com.google.gson.Gson
 class VolleyHandler(private val context: Context?) {
     private val requestQueue by lazy { Volley.newRequestQueue(context) }
     fun getWeatherData(callback: OperationalCallBackWeather) {
-         val request = StringRequest(
-             Request.Method.GET,
-             "${Constant.BASE_URL}$END_POINT_WEATHER",
-             {
-                 val response = Gson().fromJson(it, WeatherResponse::class.java)
-                 callback.onSuccess(response)
-             },
-             {
+        val request = StringRequest(
+            Request.Method.GET,
+            "${Constant.BASE_URL}$END_POINT_WEATHER",
+            {
+                val response = Gson().fromJson(it, WeatherResponse::class.java)
+                callback.onSuccess(response)
+            },
+            {
                 callback.onFailure(it.toString())
-             }
-         )
+            }
+        )
         requestQueue.add(request)
     }
 
     fun getForecastData(callback: OperationalCallbackForeCast) {
         val request = StringRequest(
             Request.Method.GET,
-            BASE_URL+END_POINT_FORECAST,
+            BASE_URL + END_POINT_FORECAST,
             {
                 try {
                     val apiResponse = Gson().fromJson(it, ForecastResponse::class.java)
@@ -51,23 +51,27 @@ class VolleyHandler(private val context: Context?) {
         requestQueue.add(request)
     }
 
-    fun getZipCodeData() {
+        fun getForecastData() {
 
-    }
+        }
 
-    fun getAirPollutionData(
-        latitude: Double,
-        longitude: Double,
-        callback: MVPAirPollution.ICallback
-    ) {
-        StringRequest(
-            Request.Method.GET,
-            Constant.BASE_URL + Constant.END_POINT_AIR_POLLUTION +
-                    "?lat=$latitude" +
-                    "&lon=$longitude" +
-                    "&appid=${Constant.API_KEY}",
-            { callback.onSuccess( Gson().fromJson(it, AirPollutionResponse::class.java) ) },
-            { callback.onError(it.message) }
-        ).also { requestQueue.add(it) }
-    }
+        fun getZipCodeData() {
+
+        }
+
+        fun getAirPollutionData(
+            latitude: Double,
+            longitude: Double,
+            callback: MVPAirPollution.ICallback
+        ) {
+            StringRequest(
+                Request.Method.GET,
+                Constant.BASE_URL + Constant.END_POINT_AIR_POLLUTION +
+                        "?lat=$latitude" +
+                        "&lon=$longitude" +
+                        "&appid=${Constant.API_KEY}",
+                { callback.onSuccess(Gson().fromJson(it, AirPollutionResponse::class.java)) },
+                { callback.onError(it.message) }
+            ).also { requestQueue.add(it) }
+        }
 }
--- app/src/main/java/com/example/weatherwidget/view/activity/MainActivity.kt
+++ app/src/main/java/com/example/weatherwidget/view/activity/MainActivity.kt
@@ -1,17 +1,11 @@
 package com.example.weatherwidget.view
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import com.example.weatherwidget.databinding.ActivityMainBinding
-import com.example.weatherwidget.model.remote.Constant
-import com.example.weatherwidget.model.remote.VolleyHandler
-import com.example.weatherwidget.model.remote.data_forecast.ForecastResponse
 import com.example.weatherwidget.presenter.mvp_forecast.ForecastPresenter
-import com.example.weatherwidget.presenter.mvp_forecast.MVPForecast
 
-class MainActivity : AppCompatActivity(),MVPForecast, MVPForecast.ForecastView {
+class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var forecastPresenter: ForecastPresenter
 
@@ -19,40 +13,6 @@ class MainActivity : AppCompatActivity(),MVPForecast, MVPForecast.ForecastView {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.btnAirPollution.setOnClickListener {
-            Intent(this, AirPollutionActivity::class.java).apply {
-                // Btw this is Long Beach, Los Angeles.
-                putExtra(
-                    AirPollutionActivity.AIR_POLLUTION_LATITUDE_ARG,
-                    Constant.PLACEHOLDER_COORDS.first
-                )
-                putExtra(
-                    AirPollutionActivity.AIR_POLLUTION_LONGITUDE_ARG,
-                    Constant.PLACEHOLDER_COORDS.second
-                )
-                startActivity(this)
-            }
-        }
-
-        initForecastPresenter()
-        binding.btnForecast.setOnClickListener {
-            forecastPresenter.getForecast()
-        }
-    }
-    private fun initForecastPresenter() {
-       forecastPresenter = ForecastPresenter(VolleyHandler(this),this)
     }
 
-    override fun onLoad(isLoading: Boolean) {
-        TODO("Not yet implemented")
-    }
-
-    override fun showError(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-    }
-
-    override fun setResult(forecastResponse: ForecastResponse) {
-        TODO("Not yet implemented")
-    }
 }
