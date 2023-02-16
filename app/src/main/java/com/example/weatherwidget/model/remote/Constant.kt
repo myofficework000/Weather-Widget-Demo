a420cb161427a8a3f590b8c116089821ec607a76
Luan Dang <luandang9697@gmail.com>
2023-02-16 10:19:01 -0600

    converted temperature to celsius and updated weather UI

--- app/src/main/java/com/example/weatherwidget/model/remote/Constant.kt
+++ app/src/main/java/com/example/weatherwidget/model/remote/Constant.kt
@@ -6,6 +6,7 @@ import android.graphics.drawable.GradientDrawable
 This class is responsible to add constant of project
 */
 object Constant {
+    const val ABSOLUTE_ZERO = 273.15
     const val BASE_URL = "https://api.openweathermap.org/"
     const val API_KEY = "979f405df6be739658dd4ebbde6eb52c"
     const val END_POINT_WEATHER = "data/2.5/weather?q=Houston&appid=$API_KEY"
