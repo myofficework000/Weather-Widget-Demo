44517e7cd70b9cb5e10eccbae2261fd35328a919
cheetahmail007 <cheetahmail007@gmail.com>
2023-02-21 20:30:56 +0530

    fixed recyclerview issue

--- app/src/main/java/com/example/weatherwidget/view/adapter/ForecastAdapter.kt
+++ app/src/main/java/com/example/weatherwidget/view/adapter/ForecastAdapter.kt
@@ -2,10 +2,13 @@ package com.example.weatherwidget.view.adapter
 
 import android.text.format.DateUtils
 import android.view.LayoutInflater
+import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.weatherwidget.R
 import com.example.weatherwidget.databinding.ForecastItemBinding
 import com.example.weatherwidget.model.remote.Constant
 import com.example.weatherwidget.model.remote.Constant.ABSOLUTE_ZERO
@@ -24,8 +27,7 @@ class ForecastAdapter(private var listOfForecast: List<Forecast>) :
     ): ForecastAdapter.ForecastViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         binding = ForecastItemBinding.inflate(layoutInflater, parent, false)
-
-        return ForecastViewHolder(binding.root)
+        return ForecastViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ForecastAdapter.ForecastViewHolder, position: Int) {
@@ -34,10 +36,14 @@ class ForecastAdapter(private var listOfForecast: List<Forecast>) :
 
     override fun getItemCount() = listOfForecast.size
 
-    inner class ForecastViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    inner class ForecastViewHolder(private val view: ForecastItemBinding) :
+        RecyclerView.ViewHolder(view.root) {
+        private lateinit var textViewDay: TextView
+
         fun bind(forecast: Forecast) {
+            textViewDay = binding.txtDay
             forecast.apply {
-                binding.txtDay.text = TimeUtil.dayStringFormat(forecast.dt_txt)
+                view.txtDay.text = TimeUtil.dayStringFormat(listOfForecast[position].dt_txt)
                 Glide.with(this@ForecastViewHolder.itemView.context)
                     .load("${IMG_URL}/${forecast.weather[0].icon}.png")
                     .into(binding.imgForecast)
